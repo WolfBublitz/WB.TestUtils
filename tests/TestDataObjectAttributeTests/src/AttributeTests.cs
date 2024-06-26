4318fb6949397c76f2cc012f4f12d8a31af3ac47
Wolf Bublitz <wolf.bublitz@gmail.com>
2024-06-26 13:35:46 +0200

    updated tests

--- tests/TestDataObjectAttributeTests/src/AttributeTests.cs
+++ tests/TestDataObjectAttributeTests/src/AttributeTests.cs
@@ -4,33 +4,33 @@ namespace TestDataAttributeTests.AttributeTests;
 
 public class TestData : TestDataObjectAttribute
 {
-    public int IntValue { get; set; } = 42;
+    public int IntValue { get; set; }
 
-    public string StringValue { get; set; } = "Hello, World!";
+    public string? StringValue { get; set; }
 
-    public object ObjectValue { get; set; } = new object();
+    public object? ObjectValue { get; set; }
 }
 
 [TestClass]
 public class TheTestDataAttribute
 {
     [DataTestMethod]
-    [TestData]
+    [TestData(IntValue = 42, StringValue = "Hello World!", ObjectValue = null)]
     public void CanBeInjectedDirectory(TestData testData)
     {
         // Assert
         testData.IntValue.Should().Be(42);
-        testData.StringValue.Should().Be("Hello, World!");
-        testData.ObjectValue.Should().NotBeNull();
+        testData.StringValue.Should().Be("Hello World!");
+        testData.ObjectValue.Should().BeNull();
     }
 
     [DataTestMethod]
-    [TestData]
+    [TestData(IntValue = 42, StringValue = "Hello World!", ObjectValue = null)]
     public void ShouldProvideEachPropertyAsTestParameter(int intValue, string stringValue, object objectValue)
     {
         // Assert
         intValue.Should().Be(42);
-        stringValue.Should().Be("Hello, World!");
-        objectValue.Should().NotBeNull();
+        stringValue.Should().Be("Hello World!");
+        objectValue.Should().BeNull();
     }
 }
